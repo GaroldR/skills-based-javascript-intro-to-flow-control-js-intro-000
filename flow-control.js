63aad44c3d1bda884f0de96f9a605eb94b3d3d38
Garold Rabinovich <garold.rabinovich@gmail.com>
2021-01-10 18:32:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,31 @@
-function basicTeenager(age) {
-
+function basicTeenager (age) {
+  if (13 <= age <= 19) {
+    return 'You are a teenager!'
+  }
 }
+basicTeenager(14)
 
 function teenager(age) {
-
+  if (13 <= age <= 19) {
+    console.log ('You are a teenager!')
+  }
+    else if (12>=age>=20) {
+      console.log ('You are not a teenager.')
+      }
 }
+teenager(25)
 
 function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
-}
+  if (13 <= age <= 19) {
+    return 'You are a teenager!'
+  }
+ else if (age<=12) {
+      return 'You are a kid'
+    }
+    else if (age>=20) {
+      return 'You are a grownup'
+    }
+  }
+  ageChecker(14)
+  ageChecker(11)
+  ageChecker(26)
